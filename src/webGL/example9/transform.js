b71751eac9bc50b99e052893bdeb1cd275b0566a
Amit Tomar <Amit-Tomar@users.noreply.github.com>
2021-01-19 20:20:22 +0000

    refactor matrix operations

--- src/webGL/example9/transform.js
+++ src/webGL/example9/transform.js
@@ -4,13 +4,10 @@ export default class Transform
 {
 	constructor()
 	{
-		this.translate = vec3.create();
-		vec3.set(this.translate, 0, 0, 0);
-		this.scale = vec3.create();
-		vec3.set(this.scale, 1, 1, 1);
+		this.translate = vec3.fromValues( 0, 0, 0);
+		this.scale = vec3.fromValues( 1, 1, 1);
 		this.rotationAngle = 0;
-		this.rotationAxis = vec3.create();
-		vec3.set(this.rotationAxis, 1, 0, 0);
+		this.rotationAxis = vec3.fromValues( 0, 0, 1);
 
 		this.modelTransformMatrix = mat4.create();
 		mat4.identity(this.modelTransformMatrix);
@@ -30,8 +27,8 @@ export default class Transform
 	{
 		mat4.identity(this.modelTransformMatrix);
 		mat4.translate(this.modelTransformMatrix, this.modelTransformMatrix, this.translate);
-		mat4.scale(this.modelTransformMatrix, this.modelTransformMatrix, this.scale);
 		mat4.rotate(this.modelTransformMatrix, this.modelTransformMatrix, this.rotationAngle, this.rotationAxis);
+		mat4.scale(this.modelTransformMatrix, this.modelTransformMatrix, this.scale);
 	}
 
 	setTranslate(translationVec)
